356319198ac04192a7a979b13603e4bd5a035b93
alexander_shloma <alexander_shloma@mail.ru>
2023-10-13 21:27:48 +0300

    10 bombs

---
+++ src/sweeper/Bomb.java
@@ -0,0 +1,30 @@
+package sweeper;
+
+public class Bomb
+{
+    private Matrix bombMap;
+    private int totalBombs;
+
+    Bomb(int totalBombs)
+    {
+        this.totalBombs = totalBombs;
+    }
+
+    void start()
+    {
+        bombMap = new Matrix(Box.ZERO);
+        for (int j = 0; j < totalBombs; j++)
+            placeBomb();
+    }
+
+    Box get(Coord coord)
+    {
+        return bombMap.get(coord);
+    }
+
+    private void placeBomb()
+    {
+        Coord coord = Ranges.getRandomCoord();
+        bombMap.set(coord, Box.BOMB);
+    }
+}
